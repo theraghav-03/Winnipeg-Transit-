675e6e5247140604204662c2934353cb6749c84a
Raghav sharma <rsharma16@academic.rrc.ca>
2025-01-12 11:29:43 -0600

    some changes in layout

--- main.py
+++ main.py
@@ -3,17 +3,15 @@
 # Description : Using Winnipeg Transit API to get the nearby stops and their schedules
 
 import json
-import turtle
 import urllib.request
 from dateutil.parser import parse
-from datetime import datetime
 from colorama import Fore, Back, Style
 
 API_KEY = 'GpySP2xycRk1KCahLdcT'
  
-lon = -97.197585 # GPS longitude of 45 smithurst street
-lat = 49.936993  # GPS latitude of 45 smithurst street
-distance = 300  # radius in meters to search around GPS coordinates
+lon = -97.197585 # GPS longitude of my home
+lat = 49.936993  # GPS latitude of my home
+distance = 500  # radius in meters to search around GPS coordinates
 
  # url to request stops
 url_stops = f"https://api.winnipegtransit.com/v3/stops.json?lon={lon}&lat={lat}&distance={distance}&api-key={API_KEY}"
@@ -21,13 +19,13 @@ url_stops = f"https://api.winnipegtransit.com/v3/stops.json?lon={lon}&lat={lat}&
  
 response = urllib.request.urlopen(url_stops)
 stops = json.loads(response.read())
-
 # print(stops)
 
 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 print(f"Nearby stops within {distance}m:")
 for stop in stops['stops']:
-    print(f"Stop Number: {stop['key']} -> \n  Stop Name: {stop['name']} - {stop['distances']['direct']} meters away on {stop['street']['name']} \n")
+    
+    print(f"Stop Number: {Fore.YELLOW}{stop['key']}{Fore.RESET} :: \n  Stop Name: {stop['name']} - {stop['distances']['direct']} meters away on {stop['street']['name']} \n")
     print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
 
@@ -35,12 +33,12 @@ for stop in stops['stops']:
 stops_dict = {stop['key']: stop['name'] for stop in stops['stops']}
 # print(stops_dict)
 
-stop_number = int(input("Enter a stop number to get the schedule:\n "))
+stop_number = int(input("Enter a stop number to get the schedule:\t"))
 
 if stop_number in stops_dict:
     stop_info = stops_dict[stop_number]
     print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-")
-    print("Schedule for stop:",stop_number,"-",stop_info,":::")
+    print(f"Schedule for stop:{Fore.YELLOW}{stop_number}{Fore.RESET}-stop_info:::")
 
     #url for bus schedules
     url_schedule = f"https://api.winnipegtransit.com/v3/stops/{stop_number}/schedule.json?max-results-per-route=3&api-key={API_KEY}"
@@ -51,13 +49,14 @@ if stop_number in stops_dict:
     # print(bus_number_dict)
     
     print(f"Bus number     \t\tBus Name    \t\t\tArrival Time   \t\tEstimated Time")
+    
     for schedule in schedules['stop-schedule']['route-schedules'] :
         bus_name = schedule['route']['name']
         bus_number = schedule['route']['number']
         
         for bus in schedule['scheduled-stops']:
             arrival_time = parse(bus['times']['arrival']['scheduled'])
-            estimated_time = parse(bus['times']['arrival']['estimated'])
+            estimated_time = parse(bus['times']['arrival']['estimated']) 
             
             if estimated_time > arrival_time:
                 print(f" {bus_number} \t\t{bus_name}  \t\t {Fore.RED}{arrival_time.strftime("%I:%M %p")} \t\t {estimated_time.strftime("%I:%M %p")}\t LATE{Fore.RESET}")
